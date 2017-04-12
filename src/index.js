fee2e5ec2e3a8537e9925029bf479e5ce30e6b42
mhelvens <mhelvens@gmail.com>
2017-04-12 16:31:28 +0100

    fetching options from the right place, and converted to es5 code (just to be sure)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-const babel = require('babel-core');
+var babel = require('babel-core');
 
-exports.onHandleCode = (event) => {
-  try {
-    const result = babel.transform(event.data.code, event.data.option);
-    event.data.code = result.code;
-  } catch (error) {
-    console.error(error);
-  }
+var babelOptions;
+
+exports.onStart = function onStart(event) {
+	if (typeof event.data.option !== 'object') {
+		throw new Error(`Please supply an "option" object in the configuration of esdoc-babel-plugin.`);
+	}
+	babelOptions = event.data.option;
+};
+
+exports.onHandleCode = function onHandleCode(event) {
+	try {
+		var result      = babel.transform(event.data.code, babelOptions);
+		event.data.code = result.code;
+	} catch (error) {
+		console.error(error);
+	}
 };
